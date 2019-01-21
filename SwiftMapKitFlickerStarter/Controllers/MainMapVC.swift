5f06c0d4ff2e6de4c3c19cd90578e4a839809331
a <eahilendran@gmail.com>
2019-01-21 16:12:51 +0530

    Add MKAnnotation

--- SwiftMapKitFlickerStarter/Controllers/MainMapVC.swift
+++ SwiftMapKitFlickerStarter/Controllers/MainMapVC.swift
@@ -13,7 +13,7 @@ import CoreLocation
 import Alamofire
 import AlamofireImage
 
-class MainMapVC: UIViewController {
+class MainMapVC: UIViewController, UIGestureRecognizerDelegate {
 
     
     let locationManager = CLLocationManager()
@@ -25,9 +25,13 @@ class MainMapVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        mapView.delegate = self
         locationManager.delegate = self
-        configureLocationServices()    }
+        configureLocationServices()
+        addDoubleTap()
+        
+    }
 
     @IBAction func centerMapButtonPressed(_ sender: Any) {
         if(authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse){
@@ -35,9 +39,30 @@ class MainMapVC: UIViewController {
         }
     }
     
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        mapView.addGestureRecognizer(doubleTap)
+    }
+    
+    
 }
 
 extension MainMapVC: MKMapViewDelegate{
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation{
+            return nil
+        }
+        
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        pinAnnotation.pinTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        pinAnnotation.animatesDrop = true
+        return pinAnnotation
+        
+    }
     
     func centerMapOnUserLocation(){
         guard let coordinate = locationManager.location?.coordinate else{return}
@@ -45,6 +70,28 @@ extension MainMapVC: MKMapViewDelegate{
         let coordinateRegion = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
     }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer){
+        print("dddd")
+        removePin()
+        let touchPoint = sender.location(in: mapView)
+        let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        print("ddd",touchCoordinate.latitude)
+        
+        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
+        mapView.addAnnotation(annotation)
+        
+        let coordinateRegion = MKCoordinateRegion(center: touchCoordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    func removePin() {
+        for annotation in mapView.annotations {
+            mapView.removeAnnotation(annotation)
+        }
+    }
+    
 }
 
 extension MainMapVC: CLLocationManagerDelegate{
---
+++ SwiftMapKitFlickerStarter/View/DroppablePin.swift
@@ -0,0 +1,21 @@
+//
+//  DroppablePin.swift
+//  SwiftMapKitFlickerStarter
+//
+//  Created by MacBook on 1/21/19.
+//  Copyright © 2019 Ahil. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+    
+    init(coordinate: CLLocationCoordinate2D, identifier: String) {
+        self.coordinate = coordinate
+        self.identifier = identifier
+        super.init()
+    }
+}
