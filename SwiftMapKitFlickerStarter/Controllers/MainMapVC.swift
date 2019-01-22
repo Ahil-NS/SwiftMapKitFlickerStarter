880502d8579c27ac0f5dd8d11cfe813406c9df05
a <eahilendran@gmail.com>
2019-01-22 12:41:15 +0530

    Add Progress bar and Progress label

--- SwiftMapKitFlickerStarter/Controllers/MainMapVC.swift
+++ SwiftMapKitFlickerStarter/Controllers/MainMapVC.swift
@@ -15,14 +15,30 @@ import AlamofireImage
 
 class MainMapVC: UIViewController, UIGestureRecognizerDelegate {
 
+    private let locationManager = CLLocationManager()
+    private let authorizationStatus = CLLocationManager.authorizationStatus()
+    private let regionRadius: Double = 1000
     
-    let locationManager = CLLocationManager()
-    let authorizationStatus = CLLocationManager.authorizationStatus()
-    let regionRadius: Double = 1000
+    private var screenSize = UIScreen.main.bounds
     
+    private let spinner: UIActivityIndicatorView = {
+        let spin = UIActivityIndicatorView()
+        spin.style = .whiteLarge
+        spin.color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return spin
+    }()
     
+    private let progressLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        label.textAlignment = .center
+        return label
+    }()
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,7 +46,6 @@ class MainMapVC: UIViewController, UIGestureRecognizerDelegate {
         locationManager.delegate = self
         configureLocationServices()
         addDoubleTap()
-        
     }
 
     @IBAction func centerMapButtonPressed(_ sender: Any) {
@@ -39,11 +54,55 @@ class MainMapVC: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
-    func addDoubleTap() {
+    private func addSwipe() {
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    
+    private func addDoubleTap() {
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
         doubleTap.numberOfTapsRequired = 2
         doubleTap.delegate = self
         mapView.addGestureRecognizer(doubleTap)
+    }
+
+    
+    private func animateViewUp() {
+        pullHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func animateViewDown() {
+        pullHeightConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
+    private func showSpinner(){
+        spinner.center = CGPoint(x: (screenSize.width/2) - ((spinner.frame.width)/2), y: 150)
+        spinner.startAnimating()
+        pullUpView.addSubview(spinner)
+    }
+    
+    private func removeSpinner(){
+       spinner.removeFromSuperview()
+        print("remove spinner")
+    }
+    
+    private func showProgressLabel(){
+        progressLabel.frame = CGRect(x: (screenSize.width/2) - 100, y: 175, width: 200, height: 50)
+        progressLabel.text = "Progressing...."
+        pullUpView.addSubview(progressLabel)
+    }
+    
+    private func removeProgressLabel(){
+        progressLabel.removeFromSuperview()
     }
     
     
@@ -74,6 +133,14 @@ extension MainMapVC: MKMapViewDelegate{
     @objc func dropPin(sender: UITapGestureRecognizer){
         print("dddd")
         removePin()
+        removeSpinner()
+        removeProgressLabel()
+        
+        animateViewUp()
+        addSwipe()
+        showSpinner()
+        showProgressLabel()
+        
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         print("ddd",touchCoordinate.latitude)
